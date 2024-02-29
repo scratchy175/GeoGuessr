bef284a69bd7372a5b8d7a0de4a195d549b22252
Hachem-SH <hachemsouiher19@gmail.com>
2024-02-29 23:12:20 +0100

    2nd try to host

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -14,11 +14,11 @@ const about = () => {
 
             <div className="bg-gray-200 w-full rounded-lg shadow-md p-4 mb-4">
                 <h2 className="text-xl font-bold mb-2">A propos</h2>
-                <p>Bienvenue sur notre site de jeu de géographie, un projet développé par une équipe d'étudiants universitaires avides de découvrir le monde qui nous entoure. Notre objectif est
-                    de vous offrir une experience immersive et educative qui vous permettra d'explorer divers
+                <p>Bienvenue sur notre site de jeu de géographie, un projet développé par une équipe détudiants universitaires avides de découvrir le monde qui nous entoure. Notre objectif est
+                    de vous offrir une experience immersive et educative qui vous permettra dexplorer divers
                     endroits a travers le globe. tout en testant vos connaissances en géographie.
-                    Que vous sovez un vovageur chevronne ou un amateur curieux. notre site vous propose une aventure virtuelle captivante. Plongez dans des paysages variés, des métropoles animées aux recoins les plus reculés, et mettez vos compétences géographiques à l'epreuve en devinant votre position sur la carte.
-                    Nous croyons en l'importance de la découverte et de l'apprentissage. et c'est pourquoi nous
+                    Que vous sovez un vovageur chevronne ou un amateur curieux. notre site vous propose une aventure virtuelle captivante. Plongez dans des paysages variés, des métropoles animées aux recoins les plus reculés, et mettez vos compétences géographiques à lepreuve en devinant votre position sur la carte.
+                    Nous croyons en limportance de la découverte et de lapprentissage. et cest pourquoi nous
                     nous efforçons de rendre la géographie accessible er amusante pour tous. Notre site est constamment mis a jour avec de nouveaux défis. des fonctionnalites innovantes et des
                     contenus educatits pour ennchir votre expérience de jeu.
                     Nous sommes ravis de vous accompagner dans cette aventure géographique virtuelle, et nous espérons que notre site vous inspirera à explorer davantage notre planète fascinante
