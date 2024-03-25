a658f5a22ada0779a1affb217554f5ef33e174ef
scratchy175 <yanntopilko@free.fr>
2024-03-25 22:05:43 +0100

    db add round

---
+++ app/api/round/route.js
@@ -0,0 +1,30 @@
+import prisma from "@/helpers/prisma";
+import { NextResponse } from "next/server";
+
+// add a new round
+export async function POST(request) {
+    try {
+        const { round_id, game_id, round_nb, score, distance, time, user_point, map_point } = await request.json();
+
+        if (!round_id || !game_id || !round_nb || !score || !distance || !time || !user_point || !map_point) {
+            return NextResponse.json({ message: "All fields are required" }, { status: 400 });
+        }
+
+        const round = await prisma.round.create({
+            data: {
+                game_id,
+                round_nb,
+                score,
+                distance,
+                time,
+                user_point,
+                map_point,
+            }
+        });
+
+        return NextResponse.json({ round }, { status: 200 });
+    } catch (error) {
+        console.error("Error adding round:", error);
+        return NextResponse.json({ message: "Error adding round" }, { status: 500 });
+    }
+}
